34b12cccf12d9a2f950c8dcc8f0cbe46eff28cd9
seem <mwlorgat@gmail.com>
2021-09-05 11:06:32 +0200

    ipython: cleanup

---
+++ ipython/.ipython/profile_default/startup/startup.py
@@ -0,0 +1,40 @@
+try:
+    import ipdb
+except ModuleNotFoundError:
+    import pdb
+
+try:
+    import matplotlib.pyplot as plt
+except ModuleNotFoundError:
+    pass
+
+try:
+    import numpy as np
+except ModuleNotFoundError:
+    pass
+else:
+    np.set_printoptions(suppress=True, precision=3, linewidth=100)
+
+try:
+    import pandas as pd
+except ModuleNotFoundError:
+    pass
+else:
+    pd.options.display.width = 120
+    pd.options.display.precision = 4
+
+from datetime import datetime, timedelta
+
+from IPython import get_ipython
+ipython = get_ipython()
+if ipython is not None:
+    ipython.magic('load_ext autoreload')
+    ipython.magic('autoreload 2')
+del ipython
+
+import logging
+logger = logging.getLogger()
+logging.basicConfig(
+    level="INFO",
+    format="%(asctime)s - %(levelname)-7s - %(module)-8s - %(message)s",
+)
