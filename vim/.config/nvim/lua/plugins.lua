9503718f80d693020c8a13847e92d69d45bc7c67
seem <mwlorgat@gmail.com>
2021-09-12 08:28:17 +0200

    vim: forgot to include lua plugins + add telescope

---
+++ vim/.config/nvim/lua/plugins.lua
@@ -0,0 +1,160 @@
+return require('packer').startup(function()
+
+  use 'wbthomason/packer.nvim'
+
+  -- Basics
+
+  use 'tpope/vim-commentary'      -- Comment verbs
+  use 'tpope/vim-endwise'         -- wisely add "end"
+  use 'tpope/vim-eunuch'          -- UNIX shell commands
+  use 'tpope/vim-repeat'          -- . command for plugins
+  use 'tpope/vim-rsi'             -- Emacs keys in command mode
+  use 'tpope/vim-surround'        -- Objects and verbs for "surroundings"
+  use 'tpope/vim-vinegar'         -- Enhance netrw (built-in file browser)
+  use 'tpope/vim-unimpaired'      -- Convenient mappings on [* and ]*
+  use 'tpope/vim-fugitive'        -- Git interface
+  use 'tpope/vim-sleuth'          -- Infer shiftwidth and expandtab
+
+  use 'romainl/vim-cool'          -- Only highlight while searching
+
+  use {
+    'AndrewRadev/splitjoin.vim',  -- Syntax-aware line split/join 
+    config = function()
+      vim.g.splitjoin_trailing_comma = 1
+      vim.g.splitjoin_python_brackets_on_separate_lines = 1
+      vim.g.splitjoin_quiet = 1
+    end,
+  }
+
+  use {
+    'nvim-telescope/telescope.nvim',  -- Fuzzy finder
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  -- Don't actually use much but would like to
+
+  use 'tpope/vim-dispatch'        -- Manage builds
+  use 'tpope/vim-jdaddy'          -- JSON mappings
+  use 'tpope/vim-projectionist'   -- Project configuration
+  use 'tpope/vim-rhubarb'         -- GitHub for fugitive
+
+  use 'easymotion/vim-easymotion'
+
+  -- Colorschemes
+
+  use 'cocopon/iceberg.vim'
+  use 'romainl/Apprentice'
+
+  -- Language server
+
+  use {
+    'neovim/nvim-lspconfig',      -- Simplify lsp configuration
+    config = function()
+      local nvim_lsp = require('lspconfig')
+      -- Use an on_attach function to only map the following keys
+      -- after the language server attaches to the current buffer
+      local on_attach = function(client, bufnr)
+        local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+        local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+        -- Enable completion triggered by <c-x><c-o>
+        buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+        -- Mappings.
+        local opts = { noremap=true, silent=true }
+
+        -- See `:help vim.lsp.*` for documentation on any of the below functions
+        buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+        buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+        buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+        buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+        buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+        buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+        buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+        buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+        buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+        buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+        buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+        buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+        buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+        buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+        buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+        buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+        buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+      end
+
+      -- nvim-cmp supports additional completion capabilities
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+
+      -- Use a loop to conveniently call 'setup' on multiple servers and
+      -- map buffer local keybindings when the language server attaches
+      local servers = { 'pyright' }
+      for _, lsp in ipairs(servers) do
+        nvim_lsp[lsp].setup {
+          on_attach = on_attach,
+          capabilities = capabilities,
+        }
+      end
+    end,
+  }
+  use {
+    'hrsh7th/nvim-cmp',           -- Autocomplete
+    config = function()
+      vim.o.completeopt = 'menuone,noselect'
+      local cmp = require'cmp'
+      cmp.setup({
+        mapping = {
+          ['<C-p>'] = cmp.mapping.select_prev_item(),
+          ['<C-n>'] = cmp.mapping.select_next_item(),
+          ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.close(),
+          ['<CR>'] = cmp.mapping.confirm {
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+          },
+          ['<Tab>'] = function(fallback)
+            if vim.fn.pumvisible() == 1 then
+              vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<C-n>', true, true, true), 'n')
+            else
+              fallback()
+            end
+          end,
+          ['<S-Tab>'] = function(fallback)
+            if vim.fn.pumvisible() == 1 then
+              vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<C-p>', true, true, true), 'n')
+            else
+              fallback()
+            end
+          end,
+        },
+        sources = {
+          { name = 'nvim_lsp' },
+          { name = 'buffer' },
+        },
+      })
+    end,
+  }
+  use {
+    'ray-x/lsp_signature.nvim',   -- Function signature popup with arg highlighting
+    config = function()
+      require'lsp_signature'.setup({
+          bind = true, -- This is mandatory, otherwise border config won't get registered.
+          hint_prefix = "",
+          handler_opts = {
+            border = "none",
+          }
+        })
+    end,
+  }
+  -- Completion sources
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-nvim-lsp'
+
+  -- Languages
+
+  use 'hashivim/vim-terraform'
+
+end)
