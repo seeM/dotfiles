4a8e698bf2e89e8429af7d9f0de92c94d5a95567
seem <mwlorgat@gmail.com>
2021-10-31 15:10:46 +0200

    vim: trying cmp *again*

--- vim/.config/nvim/lua/plugins.lua
+++ vim/.config/nvim/lua/plugins.lua
@@ -184,41 +184,41 @@ return require('packer').startup(function()
   --     end
   --   end,
   -- }
-  -- use {
-  --   'hrsh7th/nvim-cmp',           -- Autocomplete
-  --   config = function()
-  --     vim.o.completeopt = 'menuone,noselect'
-  --     local cmp = require'cmp'
-  --     cmp.setup({
-  --       mapping = {
-  --         ['<C-p>'] = cmp.mapping.select_prev_item(),
-  --         ['<C-n>'] = cmp.mapping.select_next_item(),
-  --         ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-  --         ['<C-f>'] = cmp.mapping.scroll_docs(4),
-  --         ['<C-Space>'] = cmp.mapping.complete(),
-  --         ['<C-e>'] = cmp.mapping.close(),
-  --         ['<Tab>'] = function(fallback)
-  --           if cmp.visible() then
-  --             cmp.select_next_item()
-  --           else
-  --             fallback()
-  --           end
-  --         end,
-  --         ['<S-Tab>'] = function(fallback)
-  --           if cmp.visible() then
-  --             cmp.select_prev_item()
-  --           else
-  --             fallback()
-  --           end
-  --         end,
-  --       },
-  --       sources = {
-  --         { name = 'nvim_lsp' },
-  --         { name = 'buffer' },
-  --       },
-  --     })
-  --   end,
-  -- }
+  use {
+    'hrsh7th/nvim-cmp',           -- Autocomplete
+    config = function()
+      vim.o.completeopt = 'menuone,noselect'
+      local cmp = require'cmp'
+      cmp.setup({
+        mapping = {
+          ['<C-p>'] = cmp.mapping.select_prev_item(),
+          ['<C-n>'] = cmp.mapping.select_next_item(),
+          ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.close(),
+          ['<Tab>'] = function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            else
+              fallback()
+            end
+          end,
+          ['<S-Tab>'] = function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            else
+              fallback()
+            end
+          end,
+        },
+        sources = {
+          { name = 'nvim_lsp' },
+          { name = 'buffer' },
+        },
+      })
+    end,
+  }
   -- use {
   --   'ray-x/lsp_signature.nvim',   -- Function signature popup with arg highlighting
   --   config = function()
@@ -231,8 +231,8 @@ return require('packer').startup(function()
   --       })
   --   end,
   -- }
-  -- -- Completion sources
-  -- use 'hrsh7th/cmp-buffer'
+  -- Completion sources
+  use 'hrsh7th/cmp-buffer'
   -- use 'hrsh7th/cmp-nvim-lsp'
 
   -- Languages
