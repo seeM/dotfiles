17dd5b9dc3a591ad3dbe3f086823e11a2c0d6227
seem <mwlorgat@gmail.com>
2022-07-30 16:14:03 +1000

    jupyter

--- jupyter/.jupyter/jupyter_notebook_config.py
+++ jupyter/.jupyter/jupyter_notebook_config.py
@@ -1,6 +1,6 @@
-def nbdev_clean_pre_save(model, **kwargs):
+def nbdev_clean_pre_save(model, path=None, contents_manager=None):
     from nbdev.clean import clean_nb
     if model['type'] != 'notebook' or model['content']['nbformat'] != 4: return
     clean_nb(model['content'])
 
-c.FileContentsManager.pre_save_hook = nbdev_clean_pre_save
+c.ContentsManager.pre_save_hook = nbdev_clean_pre_save
