839a977ded010054c034852659d9882598417760
seem <mwlorgat@gmail.com>
2022-08-03 09:30:42 +1000

    bash

--- jupyter/.jupyter/jupyter_notebook_config.py
+++
@@ -1,6 +0,0 @@
-def nbdev_clean_pre_save(model, path=None, contents_manager=None):
-    from nbdev.clean import clean_nb
-    if model['type'] != 'notebook' or model['content']['nbformat'] != 4: return
-    clean_nb(model['content'])
-
-c.ContentsManager.pre_save_hook = nbdev_clean_pre_save
